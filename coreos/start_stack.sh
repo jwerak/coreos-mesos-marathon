ba1d64a31a5a7ebd683d3bf5fda41180e61952fe
Jakub Veverka <jveverka@vendavo.com>
2014-08-13 10:42:12 +0200

    Remove original static fleet unit files

--- coreos/start_stack.sh
+++
@@ -1,9 +0,0 @@
-cd units
-
-for SERVICE in zookeeper.1.service zookeepersidekick.1.service mesos-master.1.service marathon.1.service mesos-slave.1.service
-do
-	fleetctl submit $SERVICE
-	sleep 5
-	fleetctl start $SERVICE
-done
-
